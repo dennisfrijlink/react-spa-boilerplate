ab45ab1e3967e00a76aa644a6d4fbd0674d87d2c
Dennis Frijlink <dennisfrijlink@gmail.com>
2021-01-05 18:49:35 +0100

    Changed HashRouter to BrowserRouter

--- src/root.js
+++ src/root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import AppRoute from './router/AppRoute';
 
 //styles
